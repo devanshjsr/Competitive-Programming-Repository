346a222fe16d2717467afd4f62922487d7f9b392
devanshjsr <45851839+devanshjsr@users.noreply.github.com>
2020-09-30 22:35:15 +0530

    Create Moore's Voting Algorithm.java

---
+++ src/Moore's Voting Algorithm.java
@@ -0,0 +1,58 @@
+//devanshjsr
+
+static int appearsNBy3(int arr[], int n) 
+    { 
+        int count1 = 0, count2 = 0; 
+           
+        int first =  Integer.MIN_VALUE;; 
+        int second = Integer.MAX_VALUE; 
+      
+        for (int i = 0; i < n; i++) { 
+       
+            if (first == arr[i]) 
+           {
+                count1++; 
+            }
+           else if (second == arr[i]) 
+            {
+                count2++; 
+            }          
+            else if (count1 == 0) 
+           { 
+                count1++; 
+                first = arr[i]; 
+            } 
+      
+            else if (count2 == 0) 
+            { 
+                count2++; 
+                second = arr[i]; 
+            } 
+       
+            else 
+           { 
+                count1--; 
+                count2--; 
+            } 
+        } 
+      
+        count1 = 0; 
+        count2 = 0; 
+       
+        for (int i = 0; i < n; i++) 
+     { 
+            if (arr[i] == first) 
+                count1++; 
+      
+            else if (arr[i] == second) 
+                count2++; 
+        } 
+      
+        if (count1 > n / 3) 
+            return first; 
+      
+        if (count2 > n / 3) 
+            return second; 
+      
+        return -1; 
+    } 
